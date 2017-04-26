5ac4f0f1c149a14ba40985e35611823e858e0228
Alexey Plutalov <demiazz.py@gmail.com>
2017-04-26 13:38:02 +0300

    Update typings

--- d.ts/homey.d.ts
+++ d.ts/homey.d.ts
@@ -1,5 +1,5 @@
 export function query(selector: string, element?: Element): Element | null;
-export function queryAll(selector: string, element: Element): Element[];
+export function queryAll(selector: string, element?: Element): Element[];
 export function matches(selector: string, element: Element): boolean;
 export function parent(element: Element): Element | null;
 export function parents(element: Element): Element[];
