1bae4d05ef6f241ab22e71908da9fe77f3d5f538
Alexey Plutalov <demiazz.py@gmail.com>
2017-04-27 18:26:44 +0300

    Rewrite `addClass` function

--- src/addClass.js
+++ src/addClass.js
@@ -1,13 +1,15 @@
 /* @flow */
 
+import hasClass from "./hasClass";
+
 function addClass(cssClass: string, element: Element): boolean {
-  if (element.classList.contains(cssClass)) {
-    return false;
+  const result = !hasClass(cssClass, element);
+
+  if (result) {
+    element.classList.add(cssClass);
   }
 
-  element.classList.add(cssClass);
-
-  return true;
+  return result;
 }
 
 export default addClass;
