0c15f1f5147539b6063c16e69dd49d82e1e6dd36
Alexey Plutalov <demiazz.py@gmail.com>
2017-04-27 20:46:32 +0300

    Move `dataset` function to `index` module

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import parentBy from "./parentBy";
 import parents from "./parents";
 import parentsBy from "./parentsBy";
 import remove from "./remove";
-import dataset from "./dataset";
 
 /*
  *
@@ -38,9 +37,13 @@ function queryAll(selector: string, element: Element): Array<Element> {
  * Classes
  */
 
+/* ----- hasClass ----- */
+
 function hasClass(cssClass: string, element: Element): boolean {
   return element.classList.contains(cssClass);
 }
+
+/* ----- addClass ----- */
 
 function addClass(cssClass: string, element: Element): boolean {
   const result = !hasClass(cssClass, element);
@@ -52,6 +55,8 @@ function addClass(cssClass: string, element: Element): boolean {
   return result;
 }
 
+/* ----- removeClass ----- */
+
 function removeClass(cssClass: string, element: Element): boolean {
   const result = hasClass(cssClass, element);
 
@@ -61,6 +66,8 @@ function removeClass(cssClass: string, element: Element): boolean {
 
   return result;
 }
+
+/* ----- toggleClass ----- */
 
 function toggleClass(cssClass: string, element: Element): boolean {
   const result = !hasClass(cssClass, element);
@@ -72,6 +79,45 @@ function toggleClass(cssClass: string, element: Element): boolean {
   }
 
   return result;
+}
+
+/* 
+ * Dataset
+ */
+
+type Dataset = { [key: string]: string };
+type DatasetFn = (element: HTMLElement) => Dataset;
+
+function nativeDataset(element: HTMLElement): Dataset {
+  return element.dataset;
+}
+
+function polyfillDataset(element: HTMLElement): Dataset {
+  return [].slice.call(element.attributes).reduce((data, { name, value }) => {
+    if (/^data-(.+)/.test(name)) {
+      const normalizedName = name
+        .substr(5)
+        .replace(/-\w/g, str => str[1].toUpperCase());
+
+      data[normalizedName] = value;
+    }
+
+    return data;
+  }, {});
+}
+
+function getDatasetFn() {
+  const element = document.createElement("div");
+
+  return element.dataset ? nativeDataset : polyfillDataset;
+}
+
+const datasetFn: DatasetFn = getDatasetFn();
+
+/* ----- dataset ----- */
+
+function dataset(element: HTMLElement): Dataset {
+  return datasetFn(element);
 }
 
 /*
@@ -87,12 +133,13 @@ export {
   addClass,
   removeClass,
   toggleClass,
+  /* dataset */
+  dataset,
   /* other */
   matches,
   parent,
   parentBy,
   parents,
   parentsBy,
-  remove,
-  dataset
+  remove
 };
