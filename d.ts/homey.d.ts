065b47e2535657f0bdcbefdb713b7344c3ee26a6
Alexey Plutalov <demiazz.py@gmail.com>
2017-04-27 21:49:27 +0300

    Update typings

--- d.ts/homey.d.ts
+++ d.ts/homey.d.ts
@@ -1,16 +1,40 @@
 type Elements = Elements;
 type PredicateFn = (element: Element) => boolean;
 
-export function query(selector: string, element?: Element): Element | null;
-export function queryAll(selector: string, element?: Element): Elements;
+/*
+ * Queries
+ */
+
+export function query(element: Element, selector: string): Element | null;
+export function queryAll(element: Element, selector: string): Elements;
 export function matches(selector: string, element: Element): boolean;
+
+/*
+ * Classes
+ */
+
+export function hasClass(cssClass: string, element: Element): boolean;
+export function addClass(cssClass: string, element: Element): boolean;
+export function removeClass(cssClass: string, element: Element): boolean;
+export function toggleClass(cssClass: string, element: Element): boolean;
+
+/*
+ * Dataset
+ */
+
+export function dataset(element: HTMLElement): { [key: string]: string };
+
+/*
+ * Traverse
+ */
+
 export function parent(element: Element): Element | null;
 export function parentBy(selector: string | PredicateFn, element: Element): Element | null;
 export function parents(element: Element): Elements;
 export function parentsBy(selector: string | PredicateFn, element: Element): Elements;
+
+/*
+ * Manipulate
+ */
+
 export function remove(element: Element): boolean;
-export function dataset(element: HTMLElement): { [key: string]: string };
-export function addClass(cssClass: string, element: Element): boolean;
-export function removeClass(cssClass: string, element: Element): boolean;
-export function toggleClass(cssClass: string, element: Element): boolean;
-export function hasClass(cssClass: string, element: Element): boolean;
