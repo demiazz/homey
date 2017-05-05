cc3c02078ca93ee2e335e66eb208274992fdf418
Alexey Plutalov <demiazz.py@gmail.com>
2017-05-05 19:17:27 +0300

    Update TypeScript typings

--- d.ts/homey.d.ts
+++ d.ts/homey.d.ts
@@ -1,63 +1,50 @@
+/* Types */
+
 type CSSClass = string;
+type CSSSelector = string;
+type Dataset = { [key: string]: string };
 type Elements = Element[];
-type EventDetails = { [key: string]: any };
-type EventListener = (event: Event) => any;
-type EventOptions = { bubbles?: boolean, cancelable?: boolean, detail?: any };
 type EventType = string;
-type PredicateFn = (element: Element) => boolean;
-type Selector = string;
+type EventOptions = { bubbles?: boolean, cancelable?: boolean, detail?: any };
+type Predicate = (element: Element) => boolean;
 
-/*
- * Aliases
- */
+/* Aliases */
 
-export const html: Element;
 export const body: Element;
+export const html: Element;
 
-/*
- * Queries
- */
+/* CSS */
 
-export function query(element: Element, selector: Selector): Element | null;
-export function queryAll(element: Element, selector: Selector): Elements;
-export function matches(element: Element, selector: Selector): boolean;
-
-/*
- * Classes
- */
-
-export function hasClass(element: Element, cssClass: CSSClass): boolean;
 export function addClass(element: Element, cssClass: CSSClass): boolean;
+export function hasClass(element: Element, cssClass: CSSClass): boolean;
 export function removeClass(element: Element, cssClass: CSSClass): boolean;
 export function toggleClass(element: Element, cssClass: CSSClass, state?: boolean): boolean;
 
-/*
- * Dataset
- */
+/* Data */
 
-export function dataset(element: HTMLElement): { [key: string]: string };
+export function dataset(element: HTMLElement): Dataset;
 
-/*
- * Traverse
- */
+/* Events */
 
-export function parent(element: Element): Element | null;
-export function parentBy(element: Element, selector: Selector | PredicateFn): Element | null;
-export function parents(element: Element): Elements;
-export function parentsBy(element: Element, selector: Selector | PredicateFn): Elements;
-export function closest(element: Element, selector: Selector | PredicateFn): Element | null;
+export function dispatch(target: EventTarget, eventType: EventType, options?: EventOptions): boolean;
+export function on(element: Element, eventType: EventType, listener: EventListener, useCapture?: boolean): () => void;
+export function once(element: Element, eventType: EventType, listener: EventListener, useCapture?: boolean): () => void;
+export function delegate(element: Element, selector: CSSSelector, eventType: EventType, listener: EventListener, useCapture?: boolean): () => void;
 
-/*
- * Manipulate
- */
+/* Manipulation */
 
 export function remove(element: Element): boolean;
 
-/*
- * Events
- */
+/* Quering */
 
-export function on(element: Element, eventType: EventType, listener: EventListener, useCapture?: boolean): () => void;
-export function once(element: Element, eventType: EventType, listener: EventListener, useCapture?: boolean): () => void;
-export function delegate(element: Element, selector: Selector, eventType: EventType, listener: EventListener, useCapture?: boolean): () => void;
-export function dispatch(element: Element, eventType: EventType, options?: EventOptions): boolean;
+export function query(element: Element, selector: CSSSelector): Element | null;
+export function queryAll(element: Element, selector: CSSSelector): Elements;
+
+/* Traversing */
+
+export function closest(element: Element, condition: CSSSelector | Predicate): Element | null;
+export function matches(element: Element, selector: CSSSelector): boolean;
+export function parent(element: Element): Element | null;
+export function parentBy(element: Element, condition: CSSSelector | Predicate): Element | null;
+export function parents(element: Element): Elements;
+export function parentsBy(element: Element, condition: CSSSelector | Predicate): Elements;
