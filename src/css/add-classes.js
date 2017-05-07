63533eb1bb47835eb3e2c565e0cc86d635f988b8
Alexey Plutalov <demiazz.py@gmail.com>
2017-05-08 00:21:53 +0300

    :cop:

--- src/css/add-classes.js
+++ src/css/add-classes.js
@@ -4,7 +4,7 @@ import type { CSSClass } from "./types";
 
 import addClass from "./add-class";
 
-function addClasses(element: Element, ...cssClasses: Array<CSSClass>) {
+function addClasses(element: Element, ...cssClasses: Array<CSSClass>): boolean {
   return cssClasses.reduce(
     (result, cssClass) => addClass(element, cssClass) || result,
     false
