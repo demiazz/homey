145efa8e34459e1d4ba2bf58e5586542006242f3
Alexey Plutalov <demiazz.py@gmail.com>
2017-06-07 10:32:46 +0300

    Add spec helpers for extracting text nodes with content and element nodes

--- build/jasmine/helpers.js
+++ build/jasmine/helpers.js
@@ -1,7 +1,40 @@
-window.createEvent = function createEvent(type) {
+function createEvent(type) {
   const event = document.createEvent("HTMLEvents");
 
   event.initEvent(type, true, true);
 
   return event;
-};
+}
+
+function filterNodes(nodes, predicate) {
+  const filteredNodes = [];
+
+  for (let i = 0; i < nodes.length; i += 1) {
+    const node = nodes.item(i);
+
+    if (predicate(node)) {
+      filteredNodes.push(node);
+    }
+  }
+
+  return filteredNodes;
+}
+
+function isSignificantNode(node) {
+  if (node.nodeType === 1) {
+    return true;
+  }
+
+  if (node.nodeType === 3 && node.wholeText.trim().length > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function getSignificantNodes(element) {
+  return filterNodes(element.childNodes, isSignificantNode);
+}
+
+window.createEvent = createEvent;
+window.getSignificantNodes = getSignificantNodes;
