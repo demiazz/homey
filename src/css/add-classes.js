1eed52c1fe3e7a8752f567cd141ddeea78fc6ddf
Alexey Plutalov <demiazz.py@gmail.com>
2017-06-11 12:42:27 +0300

    Fix `add-classes` function to prevent generating polyfill code by babel

--- src/css/add-classes.js
+++ src/css/add-classes.js
@@ -1,10 +1,18 @@
 /* @flow */
+/* eslint no-redeclare: "off" */
 
 import type { CSSClass } from "./types";
 
 import addClass from "./add-class";
 
-function addClasses(element: Element, ...cssClasses: Array<CSSClass>): boolean {
+declare function addClasses(
+  element: Element,
+  ...cssClasses: Array<CSSClass>
+): boolean;
+
+function addClasses(element): boolean {
+  const cssClasses = Array.prototype.slice.call(arguments, 1);
+
   return cssClasses.reduce(
     (result, cssClass) => addClass(element, cssClass) || result,
     false
