3ce9f531d913e6d9b92ab5dc07c4f6ae312a3a7c
Alexey Plutalov <demiazz.py@gmail.com>
2017-06-11 12:35:49 +0300

    Fix `prepend` function to prevent generating polyfill code by babel

--- src/manipulation/prepend.js
+++ src/manipulation/prepend.js
@@ -1,4 +1,5 @@
 /* @flow */
+/* eslint no-redeclare: "off" */
 
 import type { Insertable } from "./types";
 
@@ -18,10 +19,14 @@ function prependNodeList(element: Element, insertable: NodeList<*>): void {
   });
 }
 
-export default function prepend(
+declare function prepend(
   element: Element,
   ...insertables: Array<Insertable>
-): void {
+): void;
+
+export default function prepend(element) {
+  const insertables = Array.prototype.slice.call(arguments, 1);
+
   insertables.reverse().forEach(insertable => {
     if (typeof insertable === "string") {
       prependString(element, insertable);
