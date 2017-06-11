451c16e390b3cef76a69db2c9798e5a5d8ea922a
Alexey Plutalov <demiazz.py@gmail.com>
2017-06-11 12:44:19 +0300

    Fix `toggle-classes` function to prevent generating polyfill code by babel

--- src/css/toggle-classes.js
+++ src/css/toggle-classes.js
@@ -1,4 +1,5 @@
 /* @flow */
+/* eslint no-redeclare: "off" */
 
 import type { CSSClass, CSSClassesMap } from "./types";
 
@@ -15,10 +16,14 @@ function toggleClassesByMap(
   }, false);
 }
 
-function toggleClasses(
+declare function toggleClasses(
   element: Element,
   ...cssClasses: Array<CSSClass | CSSClassesMap>
-): boolean {
+): boolean;
+
+function toggleClasses(element) {
+  const cssClasses = Array.prototype.slice.call(arguments, 1);
+
   return cssClasses.reduce((result, cssClass) => {
     const currentResult = typeof cssClass === "string"
       ? toggleClass(element, cssClass)
