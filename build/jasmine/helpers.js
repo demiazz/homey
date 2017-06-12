a242995dc96e83b2b1da356047781382d2eba0e4
Alexey Plutalov <demiazz.py@gmail.com>
2017-06-12 10:13:18 +0300

    Remove unused Jasmine helpers

--- build/jasmine/helpers.js
+++ build/jasmine/helpers.js
@@ -6,35 +6,4 @@ function createEvent(type) {
   return event;
 }
 
-function filterNodes(nodes, predicate) {
-  const filteredNodes = [];
-
-  for (let i = 0; i < nodes.length; i += 1) {
-    const node = nodes.item(i);
-
-    if (predicate(node)) {
-      filteredNodes.push(node);
-    }
-  }
-
-  return filteredNodes;
-}
-
-function isSignificantNode(node) {
-  if (node.nodeType === 1) {
-    return true;
-  }
-
-  if (node.nodeType === 3 && node.wholeText.trim().length > 0) {
-    return true;
-  }
-
-  return false;
-}
-
-function getSignificantNodes(element) {
-  return filterNodes(element.childNodes, isSignificantNode);
-}
-
 window.createEvent = createEvent;
-window.getSignificantNodes = getSignificantNodes;
