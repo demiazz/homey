94ab5661dece68000185bf7d6efa1c4fc794ac8a
Alexey Plutalov <demiazz.py@gmail.com>
2017-06-12 10:04:10 +0300

    :cop:

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -7,13 +7,15 @@ function datasetNative(element: HTMLElement): Dataset {
 }
 
 function datasetPolyfill(element: HTMLElement): Dataset {
-  return [].slice.call(element.attributes).reduce((data, { name, value }) => {
+  return [].slice.call(element.attributes).reduce((data, attribute) => {
+    const name = attribute.name;
+
     if (/^data-(.+)/.test(name)) {
       const normalizedName = name
         .substr(5)
         .replace(/-\w/g, str => str[1].toUpperCase());
 
-      data[normalizedName] = value;
+      data[normalizedName] = attribute.value;
     }
 
     return data;
